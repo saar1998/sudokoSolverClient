e2843d32bda7c1a1832a891f5ef3ae31a16a5440
saar <saar6719@gmail.com>
2019-10-14 15:17:43 +0300

    Add scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import Layout from "./components/layout";
+
+const app = document.getElementById('app');
+ReactDom.render(<Layout/>, app);
